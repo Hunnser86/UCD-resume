6f1898212fc92ba13f26cf0b6a2e25add30783ce
Hunnser86 <robhunns@gmail.com>
2021-06-11 13:13:28 +0000

    changed the template id

--- assets/js/sendemail.js
+++ assets/js/sendemail.js
@@ -1,5 +1,5 @@
 function sendMail(contactForm) {
-    emailjs.send("gmail", "template_5p2hpra", {
+    emailjs.send("gmail", "rosie", {
         "from_name": contactForm.name.value,
         "from_email": contactForm.emailaddress.value,
         "project_request": contactForm.projectsummary.value
